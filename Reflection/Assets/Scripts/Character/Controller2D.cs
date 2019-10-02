4f5cda472c8a47c0612735cc833b787fd74ac7e3
Zoroiscrying <zoroiscrying@outlook.com>
2019-10-02 22:53:05 +0800

    UpdatedCharacterMovement
    AndPostProcessing LWRP Cinemachine

---
+++ Reflection/Assets/Scripts/Character/Controller2D.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : RaycastController
+{
+
+	public CollisionInfo collisions;
+	[HideInInspector]
+	public Vector2 playerInput;
+
+	public override void Start() {
+		base.Start ();
+		collisions.faceDir = 1;
+
+	}
+
+	public void Move(Vector2 moveAmount, bool standingOnPlatform) {
+		Move (moveAmount, Vector2.zero, standingOnPlatform);
+	}
+
+	public void Move(Vector2 moveAmount, Vector2 input, bool standingOnPlatform = false) {
+		UpdateRaycastOrigins ();
+
+		collisions.Reset ();
+		collisions.moveAmountOld = moveAmount;
+		playerInput = input;
+
+		if (moveAmount.x != 0) {
+			collisions.faceDir = (int)Mathf.Sign(moveAmount.x);
+		}
+
+		HorizontalCollisions (ref moveAmount);
+		if (moveAmount.y != 0) {
+			VerticalCollisions (ref moveAmount);
+		}
+
+		transform.Translate (moveAmount);
+
+		if (standingOnPlatform) {
+			collisions.below = true;
+		}
+	}
+
+	void HorizontalCollisions(ref Vector2 moveAmount) {
+		float directionX = collisions.faceDir;
+		float rayLength = Mathf.Abs (moveAmount.x) + skinWidth;
+
+		if (Mathf.Abs(moveAmount.x) < skinWidth) {
+			rayLength = 2*skinWidth;
+		}
+
+		for (int i = 0; i < horizontalRayCount; i ++) {
+			Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+			Debug.DrawRay(rayOrigin, Vector2.right * directionX,Color.red);
+
+			if (hit) 
+			{
+				if (hit.distance == 0) {
+					continue;
+				}
+
+				moveAmount.x = (hit.distance - skinWidth) * directionX;
+				rayLength = hit.distance;
+
+
+				collisions.left = directionX == -1;
+				collisions.right = directionX == 1;
+				
+			}
+		}
+	}
+
+	void VerticalCollisions(ref Vector2 moveAmount) {
+		float directionY = Mathf.Sign (moveAmount.y);
+		float rayLength = Mathf.Abs (moveAmount.y) + skinWidth;
+		
+			
+		if (Mathf.Abs(moveAmount.y) < skinWidth) {
+			rayLength = 2*skinWidth;
+		}
+
+		for (int i = 0; i < verticalRayCount; i ++) {
+
+			Vector2 rayOrigin = (directionY == -1)?raycastOrigins.bottomLeft:raycastOrigins.topLeft;
+			rayOrigin += Vector2.right * (verticalRaySpacing * i + moveAmount.x);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+			Debug.DrawRay(rayOrigin, Vector2.up * directionY,Color.red);
+
+				if (hit) 
+				{
+					if (hit.distance == 0) 
+					{
+						continue;
+					}
+					moveAmount.y = (hit.distance - skinWidth) * directionY;
+					rayLength = hit.distance;
+					collisions.below = directionY == -1;
+					collisions.above = directionY == 1;
+				}
+			}
+		}
+	
+	
+}
+
+
+
+
+
+	public struct CollisionInfo {
+		public bool above, below;
+		public bool left, right;
+		public bool descendingSlope;
+		public bool slidingDownMaxSlope;
+
+		public float slopeAngle, slopeAngleOld;
+		public Vector2 slopeNormal;
+		public Vector2 moveAmountOld;
+		public int faceDir;
+		public bool fallingThroughPlatform;
+
+		public void Reset() {
+			above = below = false;
+			left = right = false;
+			descendingSlope = false;
+			slidingDownMaxSlope = false;
+			slopeNormal = Vector2.zero;
+
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0;
+		}
+	}
+
---
+++ Reflection/Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Vector2 _playerMoveDir;
+    private Vector2 _playerMoveVelocity;
+
+	float accelerationTimeGrounded = .1f;
+	public float moveSpeed = 2;
+
+	Vector3 velocity;
+	private float velocityXSmoothing;
+	private float velocityYSmoothing;
+
+	Controller2D controller;
+
+	Vector2 directionalInput;
+
+	void Start() {
+		controller = GetComponent<Controller2D> ();
+	}
+
+	private void FixedUpdate()
+	{
+		CalculateVelocity ();
+		
+		
+		controller.Move (velocity * Time.deltaTime, directionalInput);
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;	
+		}
+
+		if (controller.collisions.left || controller.collisions.right)
+		{
+			velocity.x = 0;
+		}
+	}
+
+	private void Update()
+	{
+		//
+	}
+
+
+	public void SetDirectionalInput (Vector2 input) {
+		directionalInput = input;
+	}
+	
+
+	void CalculateVelocity() {
+		float targetVelocityX = directionalInput.x * moveSpeed;
+		float targetVelocityY = directionalInput.y * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
+		velocity.y = Mathf.SmoothDamp (velocity.y, targetVelocityY, ref velocityYSmoothing, accelerationTimeGrounded);
+		
+		//TODO::Round the whole velocity.
+		
+	}
+}
