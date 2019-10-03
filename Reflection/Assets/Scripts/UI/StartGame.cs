f6aed7f75986b71ec1e46bb8a65e0f92d7c05692
ColdCastlee <1147674832@qq.com>
2019-10-03 22:19:14 +0800

    UI

---
+++ Reflection/Assets/Scripts/UI/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+
+    public Button startButton;
+    void Start()
+    {
+        startButton.onClick.AddListener(() => { Application.LoadLevel(0); }); 
+    }
+
+}
