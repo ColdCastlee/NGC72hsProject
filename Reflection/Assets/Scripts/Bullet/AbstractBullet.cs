83cf3d9c54e8f41db7e9f3f945bbbfa453cec5f0
Zoroiscrying <zoroiscrying@outlook.com>
2019-10-03 12:16:57 +0800

    Mirror Boss

--- Reflection/Assets/Scripts/Bullet/AbstractBullet.cs
+++ Reflection/Assets/Scripts/Bullet/AbstractBullet.cs
@@ -1,29 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Bullet
 {
     public abstract class AbstractBullet : MonoBehaviour
     {
         //攻击伤害
-        public float BulletAtkDamage;
+        public int BulletAtkDamage;
         //水平或垂直的最大移动速度
         public float MaxMoveSpeedXy;
         //子弹实际移动的方向
         public Vector2 BulletMoveDir;
         //子弹实际移动的速度（带方向，它等于speed * dir，但有可能在其他子弹的设计中，改变这个最简单的计算方式）
-        public Vector2 BulletMoveVelocity;
+        [FormerlySerializedAs("BulletMoveVelocity")] public Vector2 BulletDeltaMovement;
 
 
         public LayerMask MirrorLayerMask;
         public LayerMask PlayerLayerMask;
         public LayerMask ZombieLayerMask;
         public LayerMask BossLayerMask;
+        public LayerMask WallLayerMask;
 
         public virtual void Start()
         {
-            BulletAtkDamage = 1;
-            MaxMoveSpeedXy = 1;
-            BulletMoveDir = Vector2.zero;
         }
     
         public virtual void Update()
@@ -44,7 +43,8 @@ namespace Bullet
     
         //碰撞事件
         public abstract void OnTriggerEnter2D(Collider2D hitTarget);
-    
-    
+
+        public abstract void Die();
+
     }
 }
--- Reflection/Assets/Scripts/Mirror.cs
+++ Reflection/Assets/Scripts/Mirror.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Mirror : MonoBehaviour
+public class Mirror : AbstractActorHealth
 {
     private Vector2 _reflectionDir = Vector2.right;
+    public UnityEvent OnMirrorBroken;
     [SerializeField]
     private LineRenderer _line;
+
+    public Vector2 Debug_ReflectionDir;
     
     public Vector2 ReflectionDir => _reflectionDir;
 
     private void Start()
     {
         _line = GetComponentInChildren<LineRenderer>();
+        this.Hp = 3;
     }
 
     public void Init(Vector2 reflectionDir)
@@ -23,12 +28,26 @@ public class Mirror : MonoBehaviour
     public void ShowReflectionDir()
     {
         _line.positionCount = 2;
-        _line.SetPosition(0,this.transform.position);
-        _line.SetPosition(1,this.transform.position + new Vector3(_reflectionDir.x,_reflectionDir.y,0) * 2.0f);
+        _line.SetPosition(0,Vector3.zero);
+        _line.SetPosition(1,new Vector3(_reflectionDir.x, _reflectionDir.y, 0) * 0.2f);
+        _line.startWidth = _line.endWidth = 0.02f;
+        _line.startColor = _line.endColor = Color.cyan;
     }
 
     private void Update()
     {
         ShowReflectionDir();
+        Debug_ReflectionDir = _reflectionDir;
     }
+
+    public override void CheckDeath()
+    {
+        if (!IsDied())
+        {
+            return;
+        }   
+        OnMirrorBroken.Invoke();
+        Destroy(this.gameObject);
+    }
+
 }
