cf500cbcd7d9dafb6ee644c2c6df2d4aa9e4c68f
ColdCastlee <1147674832@qq.com>
2019-10-05 01:01:57 +0800

    Audio

--- Reflection/Assets/Scripts/UI/StartGame.cs
+++ Reflection/Assets/Scripts/UI/StartGame.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UI;
-
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using ReadyGamerOne.MemorySystem;
 public class StartGame : MonoBehaviour
 {
 
     public Button startButton;
+
+    private void Awake()
+    {
+        var play = this.gameObject.AddComponent<AudioMgr>();
+    }
     void Start()
     {
+        MemoryMgr.LoadAssetFromResourceDir<AudioClip>(typeof(AudioName),"Audio/",(name,clip)=>
+        {
+            if(AudioMgr.Instance.audioclips.ContainsKey(name)==false)
+                AudioMgr.Instance.audioclips.Add(name, clip);
+        });
+        
+        
+        AudioMgr.Instance.PlayBgm(AudioName._1);
+        
         Global.GetInstance().loadName = "TestCharacterMovementScen";
-        startButton.onClick.AddListener(() => { Application.LoadLevel("loadingScene"); });
-    }
+        startButton.onClick.AddListener(() =>
+        {
+            
+            Application.LoadLevel("loadingScene");
+        });
 
+
+    }
+    
 }
--- Reflection/Assets/Scripts/UI/loadingScene.cs
+++ Reflection/Assets/Scripts/UI/loadingScene.cs
@@ -1,43 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class loadingScene : MonoBehaviour{
   
     public Slider mProgress;
     private AsyncOperation async;
-    void Start () {  
+    private float timer = 0;
+    private float target;
+    void Start () {
         StartCoroutine(loadScene());  
     }  
-      
+    
+    
     IEnumerator loadScene()  
     {  
         async = Application.LoadLevelAsync(Global.GetInstance().loadName);  
-        int displayProgress = 0;
-        int toProgress = 0;
-        async.allowSceneActivation = false;
-        while(async.progress < 0.9f) {
-            toProgress = (int)async.progress * 100;
-            while(displayProgress < toProgress) {
-                ++displayProgress;
-                SetLoadingPercentage(displayProgress);
-                yield return new WaitForEndOfFrame();
-            }
-        }
- 
-        toProgress = 100;
-        while(displayProgress < toProgress){
-            ++displayProgress;
-            SetLoadingPercentage(displayProgress);
+        yield return new WaitForEndOfFrame();   //等待帧结束
+        AsyncOperation asyncOperation =  SceneManager.LoadSceneAsync(1);   //异步加载场景API，返回异步参数
+        asyncOperation.allowSceneActivation = false;   //设置不允许加载完成后自动跳转界面
+        while (!asyncOperation.isDone)       //是否加载完成
+        { 
+            target = asyncOperation.progress;          //  加载进度
+            mProgress.value = Mathf.Lerp(mProgress.value, target, 0.1f);					//fill均匀增加
             yield return new WaitForEndOfFrame();
+
+            timer += Time.deltaTime;               //计时器
+            if (timer > 4f)
+            {
+                asyncOperation.allowSceneActivation = true;           //四秒后进入场景
+            }
+                
+
         }
-        async.allowSceneActivation = true;
-
     }
+    
 
-    void SetLoadingPercentage(int displayProgress)
-    {
-        mProgress.value = displayProgress;
-    }
 }
